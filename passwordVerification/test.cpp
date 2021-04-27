3a2d44f87d6582a8bd765bdfaf17a84c2a085236
ivancette81 <ivancette81@gmail.com>
2021-04-27 15:37:59 +0200

    nullPasswod test added.

--- passwordVerification/test.cpp
+++ passwordVerification/test.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
+/*
+* null password
+* one character password
+* eigth character password
+* >eigth character password
+* >0 upper case characters
+* >0 lower characters password
+* >0 number charaters password
+*/
+
+class PasswordVerifier
+{
+public:
+	PasswordVerifier() {};
+	bool verify(const char * password) {
+		return false;
+	}
+};
+
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+
+TEST(TestCasePasswordVerification, nullPassword) {
+	PasswordVerifier passVerifier;
+	EXPECT_FALSE(passVerifier.verify(NULL));
 }
